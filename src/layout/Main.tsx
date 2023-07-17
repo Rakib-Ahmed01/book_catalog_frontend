ca5d5f110cebcd2d75dbdaccf90f72eb4cf0c611
RakibAhmed01 <rakibahmed34360@gmail.com>
2023-07-17 19:19:53 +0600

    feat: complete project

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,14 +1,22 @@
 import { Box } from "@mantine/core"
 import { Outlet } from "react-router-dom"
+import { FooterLinks } from "../components/shared/Footer"
 import { HeaderMegaMenu } from "../components/shared/Header"
 
 export default function Main() {
   return (
     <>
       <HeaderMegaMenu />
-      <Box px={12}>
+      <Box my={16} mih={"50vh"}>
         <Outlet />
       </Box>
+      <FooterLinks
+        data={[
+          { title: "Facebook", links: [] },
+          { title: "Twitter", links: [] },
+          { title: "Instagram", links: [] },
+        ]}
+      />
     </>
   )
 }
